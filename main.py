f7fcb7fb4c9fa99601235549177acb9729369717
michaeld <michael.c.delisio@gmail.com>
2019-10-27 19:47:15 -0400

    Add main.py and test.py
    Define the execution entry point for Attach 4 game in the main.py file
    specifically, the init function. Provide skeleton structure for the test
    suite in test.py. Implementation of test cases omitted until implementation
    of program is complete.

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+=== Module description ===
+The main.py file general job is to define the entry point
+for the execution path of the Attach 4 game. The init() function
+is called upon execution of this file, which initializes a GUI
+object and displays the main screen (which starts the games loop).
+"""
+# Import classes from project files
+# uncomment later
+# from gui.py import *
+# from model.py import *
+
+
+def init() -> None:
+    """When init is invoked it initializes a new GUI
+    object and invokes the method to display the
+    main menu of the Attach 4 game.
+    """
+
+    # TODO: initialize the GUI object for this game
+
+    # TODO: invoke GUI method to start game loop - display main menu
+
+    return None
+
+
+
+# Initializes the entry point of the Attach 4 game
+if __name__ == "__main__":
+    init()
+
