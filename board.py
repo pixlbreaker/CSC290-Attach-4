e43c89bde9aa0df2ec43c3a0796d4eb595cda1fb
Tomasz Cieslak <tomasz.cieslak@mail.utoronto.ca>
2019-10-28 12:57:41 -0400

    Add documentation and type annotations
    New methods with documentation are created for the board object.
    Type annotations are updated for existing methods.

--- board.py
+++ board.py
@@ -21,11 +21,16 @@ class Board:
     #   the width of the grid
     # _grid
     #   the grid representation of the board
+    #   Example of an empty 3x3 grid:
+    #   [ ['', '', '' ],
+    #     ['', '', '' ],
+    #     ['', '', '' ] ]
+
     _n: int
     _m: int
     _grid: List[List]
 
-    def __init__(self, n, m):
+    def __init__(self, n: int, m: int):
         """
         Board with a grid of dimensions nxm and all cells
         initialized as EMPTY
@@ -33,7 +38,6 @@ class Board:
         Note:
             Grid symbols are represented as letters defined by RED or BLUE
             The empty space is represented as EMPTY
-
         """
         self._n = n
         self._m = m
@@ -42,4 +46,46 @@ class Board:
             self._grid.append([])
             for width in range(self._m):
                 self._grid[height].append(EMPTY)
+
+    def drop_piece(self, col: int) -> bool:
+        """
+        Returns true if the piece can be dropped in the given column.
+
+        Note:
+            If the column is not full the piece must drop to the top
+            most available row in the given column.
+            If the entire column is already full the drop cannot occur.
+        """
+        pass
+
+    def is_connected(self) -> bool:
+        """
+        Returns true iff the board contains a diagonal, vertical or a horizontal
+        linear combination with 4 or more pieces of the same color.
+        """
+        pass
+
+    def is_board_full(self) -> bool:
+        """
+        Returns true iff there are no EMPTY cells in the grid
+
+        Precondition:
+            There are no EMPTY cells underneath a piece
+        """
+        for col in range(self._m):
+            if self._grid[0][col] == EMPTY: # Only check if the top row is EMPTY
+                return False
+        return True
+
+    def get_piece(self, row: int, col: int) -> str:
+        """
+        Returns the piece that the grid contains at the given row, column
+        """
+        return self._grid[row][col]
+
+    def get_board(self) -> List[List]:
+        """
+        Returns the grid representation of the board
+        """
+        return self._grid
 
