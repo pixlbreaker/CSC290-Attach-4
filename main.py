59bc76679874325180f7ec42743438682847b7fd
Tomasz Cieslak <tomasz.cieslak@mail.utoronto.ca>
2019-11-27 12:44:26 -0500

    Update main and constants
    Added documentation and game constants for the game gui

--- main.py
+++ main.py
@@ -6,24 +6,8 @@ is called upon execution of this file, which initializes a GUI
 object and displays the main screen (which starts the games loop).
 """
 # Import classes from project files
-# uncomment later
-from src.gui import *
-# from model.py import *
-
-
-def init() -> None:
-    """When init is invoked it initializes a new GUI
-    object and invokes the method to display the
-    main menu of the Attach 4 game.
-    """
-
-    # Creates the game object
-    game = Gui()
-
-    return None
-
+from src.gui import Gui
 
 # Initializes the entry point of the Attach 4 game
 if __name__ == "__main__":
-    init()
-
+    Gui()
---
+++ src/util.py
@@ -0,0 +1,17 @@
+"""
+=== Module description ===
+This module's job is to define constants that 
+are used throughout the code in this folder
+"""
+
+BACKGROUND_COLOR = (0, 40, 66)  # (184, 205, 255)
+BOARD_COLOR = (23, 0, 153)
+OUTLINE_COLOR = (31, 0, 209)
+PLAYER1_GHOST_COLOR = (255, 100, 140)
+PLAYER2_GHOST_COLOR = (255, 234, 140)
+
+
+EMPTY_COLOR = (12, 0, 82)  # (255, 255, 255)
+PLAYER1_COLOR = (255, 0, 0)
+PLAYER2_COLOR = (255, 204, 0)
+SQ = 100
