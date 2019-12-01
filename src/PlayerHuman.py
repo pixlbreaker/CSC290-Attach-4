7bb637713086ab6d55960415e1e53653fcc87ec9
Niral Patel <niralutm.patel@mail.utoronto.ca>
2019-12-01 18:01:21 -0500

    Remove redundant code

--- src/PlayerHuman.py
+++ src/PlayerHuman.py
@@ -2,8 +2,8 @@
 === Module Description ===
 This module contains a class for a human player.
 """
-from src import Player
-from src import board
+from src.Player import Player
+from src.board import Board
 
 
 class PlayerHuman(Player):
@@ -19,9 +19,9 @@ class PlayerHuman(Player):
     # _game_board
     #   the board that this player is playing on.
     colour: str
-    _game_board: board
+    _game_board: Board
 
-    def __init__(self, game_board: board, colour: str):
+    def __init__(self, game_board: Board, colour: str):
         """
         Creates a new human player with the specified disc colour
         and game board.
@@ -35,13 +35,3 @@ class PlayerHuman(Player):
         column then returns whether the move was successful.
         """
         return self._game_board.drop_piece(self.colour, column)
-
-    def get_colour(self) -> str:
-        """
-        Returns the colour of this player's discs.
-        """
-        return self.colour
-
-if __name__ == '__main__':
-    b = board(8, 8)
-    a = PlayerHuman(b, "red")
